1c07ea348d6a81cb0c95ce375a6143ba4c1950bc
Michael Stöger <mstoeger@live.at>
2016-04-05 14:15:22 +0200

    Minor change

--- src/client/WriteCallback.java
+++ src/client/WriteCallback.java
@@ -1,7 +1,11 @@
 package client;
 
 import java.io.Serializable;
-
+/**
+ * Callback which prints out the result
+ * @author Michael Stoeger
+ * @version 5.4.2016
+ */
 public class WriteCallback implements Callback<String>,Serializable{
 	public void callback(String out){
 		System.out.println(out);
